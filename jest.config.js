f18026feb9498124fe8beb59b9df55b84b3055ea
Jiahao Li <lijiahao_shandong@163.com>
2021-07-06 23:14:33 +0800

    chore: update jest

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,10 @@ module.exports = {
   setupFilesAfterEnv: ['./__tests__/jest.setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   collectCoverageFrom: ['src/**/*.ts?(x)', '!src/index.tsx', '!src/types/*.ts'],
-  testMatch: ['<rootDir>/__tests__/**/*.spec.ts?(x)'],
+  testMatch: ['<rootDir>/__tests__/**/*.(spec|test).ts?(x)'],
   coverageProvider: 'v8',
   transform: {
-    '^.+\\.(jsx?|tsx?)$': 'babel-jest',
+    '^.+\\.(js|ts)x?$': 'babel-jest',
     '^.+\\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$': 'jest-transform-stub',
   },
   moduleNameMapper: {
